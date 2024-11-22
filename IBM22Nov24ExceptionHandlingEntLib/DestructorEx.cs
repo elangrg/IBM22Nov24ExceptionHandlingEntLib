681df0052a24d91011d48854c7808dfbaa387d31
EZLLeno <EZLLeno@EZL-LEN>
2024-11-22 14:16:47 +0530

    Add project files.

---
+++ IBM22Nov24ExceptionHandlingEntLib/DestructorEx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM22Nov24ExceptionHandlingEntLib
+{
+    internal class DestructorEx
+    {
+        static void Main() 
+        {
+
+            Employee obj = new Employee();
+            obj = null;
+
+            GC.WaitForPendingFinalizers();
+
+            GC.Collect();
+
+
+             obj = new Employee();
+            obj.Dispose();
+
+
+
+            using (obj= new Employee ())
+            {
+
+                Console.WriteLine(  obj.EmpID);
+            }
+
+
+            obj = null;
+
+            Console.WriteLine("End of Main");
+            Console.ReadKey();
+
+
+
+        }
+
+    }
+
+
+
+    class Employee:IDisposable
+    {
+        private bool disposedValue;
+
+        public int EmpID
+        { get; set; }
+
+
+        ~Employee()
+        {
+            Console.WriteLine(  "Destructor Called .....");
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~Employee()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Console.WriteLine(  "Disposed Called ....");
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+
+
+    class dept
+
+    {
+
+    }
+}
---
+++ IBM22Nov24ExceptionHandlingEntLib/ExceptionHandling.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBM22Nov24ExceptionHandlingEntLib
+{
+    internal class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            try
+            {
+
+                try
+                {
+                    Console.Write("Enter Number:"); i = int.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Specific :" + e.Message);
+
+
+                    // sce 1 - Recovered
+
+                    // sce 2 - Not Recovered Forward Exception 
+
+                    // throw;
+
+                    // scenario 3  report Current Failure Along with the Recieved Exception
+
+                    throw new ArgumentNullException("Argument Is Invalid...", e);
+                }
+                finally 
+                {
+                    Console.WriteLine( "Inner: Finally Called ");
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Specific :" + e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("General :" + e.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("General : Error Occured");
+            }
+            finally
+            {
+                Console.WriteLine("Outer: Finally Called ");
+            }
+
+
+
+            Console.WriteLine(  "You Entered :" + i);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
